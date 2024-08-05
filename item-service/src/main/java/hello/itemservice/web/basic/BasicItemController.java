dd4c1fa40d969abeac4cd053424c09f696f041d9
junghyunseok <jhyunseok325@gmail.com>
2024-08-06 00:27:04 +0900

    상품 수정

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -96,6 +96,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+
+        return "redirect:/basic/items/{itemId}"; //redirect return값의 itemId는 @PathVariable에 있는것도 사용 가능
+    }
+
     /**
      * 테스트용 데이터 추가
      */

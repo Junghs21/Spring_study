838012e3ff31cc3afdc9183162ff5064a8682c25
junghyunseok <jhyunseok325@gmail.com>
2024-08-22 21:47:51 +0900

    회원 등록

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {    //@Valid에서 오류가 발생하면 BindingResult에 오류가 담겨서 코드가 실행됨, BindingResult가 없다면(원래는) 코드를 실행하지 않고 에러를 반환함
+
+        if(result.hasErrors()) {
+            return "members/createMemberForm";  //스프링이 BindingResult를 이 view 화면까지 끌고가서 어떤 에러가 발생했는지 뿌려줌
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
